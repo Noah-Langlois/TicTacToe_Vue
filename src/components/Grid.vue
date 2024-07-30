a377601af66a81fd5d0009194669ab3a61e49f89
noah langlois <noahlanglois@hotmail.com>
2024-07-30 22:36:21 +0200

    Version 1.0.1 (not functionnal)

---
+++ src/components/Grid.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+const Elements = [0,1,2,3,4,5,6,7,8];
+</script>
+<template>
+    <div class="grid text-center" style="margin: 50px auto; max-width: 400px;">
+        <div class="row no-gutters">
+            <div class="case col-4" v-for="n in Elements">
+                X
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+</style>
